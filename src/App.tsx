964234b8cdc61ef462c9a136d5c0397e565870f3
Pedro Souza <pedro.olegario23@gmail.com>
2025-09-18 15:24:10 -0300

    ✨feat: validator and toasts

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './global.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes'
 import { HelmetProvider, Helmet } from 'react-helmet-async'
+import { Toaster } from "@/components/ui/sonner"
 
 export function App() {
 
   return (
     <HelmetProvider>
       <Helmet titleTemplate="%s | marketplace" />
+      <Toaster richColors/>
       <RouterProvider router={router} />
     </HelmetProvider>
   )
--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -1,13 +1,43 @@
-import { Button } from '@/components/ui/button'
-import { Helmet } from 'react-helmet-async'
 import arrowRight2 from '../../assets/icons/arrow-right-02.svg'
 import mailLetter2 from '../../assets/icons/mail-02.svg'
 import access from '../../assets/icons/access.svg'
 import view from '../../assets/icons/view.svg'
+
+import { Button } from '@/components/ui/button'
+import { Helmet } from 'react-helmet-async'
 import { Label } from '@/components/ui/label'
 import { Input } from '@/components/ui/input'
 
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { toast } from "sonner"
+
+const signInForm = z.object({
+    email: z.email(),
+    password: z.string()
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
+
+    async function handleSignIn(data: SignInForm) {
+        try {
+            await new Promise((resolve) => setTimeout(resolve, 2000))
+            console.log(data)
+            toast.success("Enviamos um link de autenticação para seu e-mail.", {
+                action: {
+                    label: "Reenviar",
+                    onClick: () => handleSignIn(data)
+                }
+            })
+        } catch (error) {
+            toast.error("Credenciais inválidas. Tente novamente.")
+        }
+    }
+
     return (
         <div className="w-[563px] h-[696px] bg-background rounded-lg">
             <Helmet title="Login" />
@@ -18,36 +48,49 @@ export function SignIn() {
                         <p className="text-sm text-muted-foreground">Informe seu e-mail e senha para entrar</p>
                     </div>
 
-                    <form className="space-y-4 w-full justify-center pt-10 pb-3">
-                        <div className="space-y-2">
-                            <Label className="text-gray-500 text-sm">E-MAIL</Label>
-                            <div className="flex items-center gap-2 border-b-1">
-                                <img src={mailLetter2} alt="" className="w-6 h-6" />
-                                <Input type="email" placeholder="Seu e-mail cadastrado" className="border-0 shadow-none" />
+                    <div className="w-full flex flex-col space-y-40">
+                        <form className="space-y-14 w-full justify-center pt-5" onSubmit={handleSubmit(handleSignIn)}>
+                            <div className="space-y-2">
+                                <Label className="text-gray-500 text-sm">E-MAIL</Label>
+                                <div className="flex items-center gap-2 border-b-1">
+                                    <img src={mailLetter2} alt="" className="w-6 h-6" />
+                                    <Input
+                                        id="email"
+                                        type="email"
+                                        placeholder="Seu e-mail cadastrado"
+                                        className="border-0 shadow-none"
+                                        {...register("email")}
+                                    />
+                                </div>
+
+                                <Label className="text-gray-500 text-sm">SENHA</Label>
+                                <div className="flex items-center gap-2 border-b-1">
+                                    <img src={access} alt="" className="w-6 h-6" />
+                                    <Input
+                                        id="password"
+                                        type="password"
+                                        placeholder="Sua senha de acesso"
+                                        className="border-0 shadow-none"
+                                        {...register("password")} />
+                                    <img src={view} alt="" className="w-6 h-6" />
+                                </div>
                             </div>
 
-                            <Label className="text-gray-500 text-sm">SENHA</Label>
-                            <div className="flex items-center gap-2 border-b-1">
-                                <img src={access} alt="" className="w-6 h-6" />
-                                <Input type="password" placeholder="Sua senha de acesso" className="border-0 shadow-none"></Input>
-                                <img src={view} alt="" className="w-6 h-6" />
-                            </div>
-                        </div>
+                            <Button disabled={isSubmitting} className="w-full h-[44px] justify-between" type="submit">
+                                <p className="text-white">Acessar</p>
+                                <img src={arrowRight2} alt="arrow-right" className="w-6 h-6 text-white" />
+                            </Button>
+                        </form>
 
-                    </form>
+                        <footer className="w-full h-[44px] flex flex-col space-y-3">
+                            <p className="text-gray-500 text-md">Ainda não tem uma conta?</p>
+                            <Button className="w-full h-[44px] justify-between" variant="outline">
+                                <p className="text-orange-500">Cadastrar</p>
+                                <img src={arrowRight2} alt="arrow-right" className="w-6 h-6" />
+                            </Button>
+                        </footer>
+                    </div>
 
-                    <Button className="w-full h-[44px] justify-between" type="submit">
-                        <p className="text-white">Acessar</p>
-                        <img src={arrowRight2} alt="arrow-right" className="w-6 h-6 text-white" />
-                    </Button>
-
-                    <footer className="w-full h-[44px] justify-between mt-30">
-                        <p className="text-gray-500 text-md">Ainda não tem uma conta?</p>
-                        <Button className="w-full h-[44px] justify-between">
-                            <p>Cadastrar</p>
-                            <img src={arrowRight2} alt="arrow-right" className="w-6 h-6" />
-                        </Button>
-                    </footer>
                 </div>
             </div>
         </div>
